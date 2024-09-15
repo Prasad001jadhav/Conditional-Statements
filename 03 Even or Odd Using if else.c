879edf6d96475bbc89fb4a42b1e33c3bff3456e8
Prasad_jadhav_01 <144381314+Prasad001jadhav@users.noreply.github.com>
2024-09-15 18:11:36 +0530

    Add files via upload

---
+++ 03 Even or Odd Using if else.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no = 0;
+
+    printf("\n Enter a Number = ");
+    scanf("%d",&no);
+
+    if(no <= 0)
+    {
+        printf("\n Invalid input.");
+        return -1;
+    }
+    if(no % 2 == 0)
+    {
+        printf("\n %d is even.",no);
+    }
+    else
+    {
+        printf("\n %d is odd.",no);
+    }
+    printf("\n");
+
+    getch();
+    return 0;
+}
