8f013ac78da2c98d656f8bb48620bb49b22074ee
Prasad_jadhav_01 <144381314+Prasad001jadhav@users.noreply.github.com>
2024-09-17 20:53:01 +0530

    Add files via upload

---
+++ 08 FInd of Cube of Given Number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int no=0;
+
+    printf("\n Enter a Number = ");
+    scanf("%d",&no);
+
+    printf("\n Cube of %d is = %d.",no,(no*no*no));
+
+    getch();
+    return 0;
+}
